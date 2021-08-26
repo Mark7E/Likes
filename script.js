cc27947d8d3495b73422e2acbdff6dd70fe16b27
Mark Eli <medo963sy@gmail.com>
2021-08-26 14:11:31 -0700

    finish

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function Like(element){
 alert ('Ninja was liked')
 
 }
+
+function count(Element){
+
+    var count = parseInt(document.querySelector("#count").innerText);
+    document.querySelector("#count").innerText = (count+1) +' '+ 'Likes';
+
+    console.log(count)
+}
+
+// function count(Element){
+//     var x = 0;
+//     var count = parseInt (document.querySelector("#count").innerText);
+//     document.querySelector("#count").innerText = (count+1) +' '+ " Likes";
+
+// }
+
